b339509fc58577c745dee6d231a7788251705cb7
ccmansilla <carloscmansilla@gmail.com>
2022-12-10 08:51:36 -0300

    quito importacion sin usar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
 import Peliculas from './components/Peliculas';
 import Login from "./components/Login";
-import Comentarios from './components/Comentarios';
 
 function App() {
   return (
