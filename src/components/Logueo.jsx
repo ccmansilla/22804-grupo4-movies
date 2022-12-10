e0d0db0bf49620d6082f3c0a136e372d19b488f5
ferffloress <f.flores016@gmail.com>
2022-12-10 11:41:08 -0300

    modificicaciones y estilos

--- src/components/Logueo.jsx
+++ src/components/Logueo.jsx
@@ -1,60 +1,67 @@
-import React, { useState } from 'react';
+import React from 'react';
+//import { useState } from "react";
 import { app } from "../fb";
+import "../Logueo.css";
 
-const Logueo = (props) => {
+function Logueo(props) {
 
-const [isRegistrando, setIsRegistrando] = React.useState(false);
+    const [isRegistrando, setIsRegistrando] = React.useState(false);
 
-const crearUsuario = (correo, password) => {
-    app.auth().createUserWithEmailAndPassword(correo, password).then((usuarioFirebase)=>{console.log("usuario creado:", usuarioFirebase);
-    props.setUsuario(usuarioFirebase);
-});
-};
+    const crearUsuario = (correo, password) => {
+        app.auth().createUserWithEmailAndPassword(correo, password).then((usuarioFirebase) => {
+            console.log("usuario creado:", usuarioFirebase);
+            props.setUsuario(usuarioFirebase);
+        });
+    };
 
-const iniciarSesion = (correo, password) => {
-    app.auth().signInWithEmailAndPassword(correo, password).then((usuarioFirebase) => {
-console.log("sesión iniciada con:", usuarioFirebase.user);
-props.setUsuario(usuarioFirebase);
-    });
-};
+    const iniciarSesion = (correo, password) => {
+        app.auth().signInWithEmailAndPassword(correo, password).then((usuarioFirebase) => {
+            console.log("sesión iniciada con:", usuarioFirebase.user);
+            props.setUsuario(usuarioFirebase);
+        });
+    };
 
-const sumbmitHandler = (e) => {
-    e.preventDefault();
-    const correo = e.target.emailField.value;
-    const password = e.target.passwordField.value;
+    const sumbmitHandler = (e) => {
+        e.preventDefault();
+        const correo = e.target.emailField.value;
+        const password = e.target.passwordField.value;
 
-    if (isRegistrando) {
-        crearUsuario(correo, password);
-    }
+        if (isRegistrando) {
+            crearUsuario(correo, password);
+        }
 
-    if (!isRegistrando) {
-        iniciarSesion(correo, password);
-    }
-};
+        if (!isRegistrando) {
+            iniciarSesion(correo, password);
+        }
+    };
 
-    return(
-        <div>
-            <h2>{ isRegistrando ? "Registrate" : "Inicia sesión"}</h2>
+    return (
+        <div className="logueo">
+            <h2>{isRegistrando ? "Registrate" : "Inicia sesión"}</h2>
             <form onSubmit={sumbmitHandler}>
-                <label htmlFor='emailField'>Correo:</label>
-                <input type="email" id="emailField" />
-                <label htmlFor='passwordField'>Contraseña:</label>
-                <input type="password" id="passwordField" />
+                <div>
+                    <label htmlFor='emailField'>Correo:</label>
+                    <input type="email" id="emailField" />
+                </div>
+                <div>
+                    <label htmlFor='passwordField'>Contraseña:</label>
+                    <input type="password" id="passwordField" />
+                </div>
                 <button type="submit">
-                {""}
-                { isRegistrando ? "Registrate":"Inicia sesión"} {""}
+                    {""}
+                    {isRegistrando ? "Registrate" : "Inicia sesión"} {""}
                 </button>
             </form>
 
             <button onClick={() => setIsRegistrando(!isRegistrando)}>
 
-            {isRegistrando
-            ?"Si ya tenés cuenta, iniciá sesión"
-            :"No tenés cuenta? Registrate"}
-            
+                {isRegistrando
+                    ? "Si ya tenés cuenta, iniciá sesión"
+                    : "No tenés cuenta? Registrate"}
+
             </button>
         </div>
-    )
+    );
 }
 
 export default Logueo;
