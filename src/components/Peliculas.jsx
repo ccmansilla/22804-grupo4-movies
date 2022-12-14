1cee191e064e21057d4b4dec9e301b711ed1f5a7
Giannina Minano <g.isabella.md@gmail.com>
2022-12-13 22:42:22 -0300

    Arreglo de estilos: Nuevas scrollbars, banner responsive y logo en banner en lugar de título

--- src/components/Peliculas.jsx
+++ src/components/Peliculas.jsx
@@ -8,22 +8,37 @@ import Banner from "./Banner";
 const Peliculas = () => {
 	return (
 		<div className="App">
-		  <Banner/>
-		  <Row
-			title="NETFLIX ORIGINALS"
-			fetchURL={requests.fetchNetflixOrginals}
-			// isLargeRow={true}
-		  />
-		  <Row title="Trending NOW" fetchURL={requests.fetchTrending} />
-		  <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
-		  <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
-		  <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
-		  <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
-		  <Row title="Romantic Movies" fetchURL={requests.fetchRomanceMovies} />
-		  <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
-		  <Comentarios />
+			<Banner />
+			<Row
+				title="SERIES ORIGINALES DE NETFLIX"
+				fetchURL={requests.fetchNetflixOrginals}
+			/>
+			<Row title="Populares" fetchURL={requests.fetchTrending} />
+			<Row
+				title="Películas Mejor Puntuadas"
+				fetchURL={requests.fetchTopRated}
+			/>
+			<Row
+				title="Películas: Acción"
+				fetchURL={requests.fetchActionMovies}
+			/>
+			<Row
+				title="Películas: Comedia"
+				fetchURL={requests.fetchComedyMovies}
+			/>
+			<Row
+				title="Películas: Terror"
+				fetchURL={requests.fetchHorrorMovies}
+			/>
+			<Row
+				title="Películas: Románticas"
+				fetchURL={requests.fetchRomanceMovies}
+			/>
+			<Row title="Documentales" fetchURL={requests.fetchDocumentaries} />
+
+			<Comentarios />
 		</div>
-	  );
-}
+	);
+};
 
 export default Peliculas;
