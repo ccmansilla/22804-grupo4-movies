9ddf5d4adef6d9c0351f2545df1634becbbd8dbe
Linda M De Marco <maribeldemarco@gmail.com>
2022-12-26 23:09:05 -0300

    se agrega menu hamburguesa en header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,16 +7,28 @@ function Header()
 
 
     return (
-        <div className="header"> 
-            <img className="logoreacflix" src="./assets/reacflixlogo.svg"/>
-            
-            <Link to="" className="links" style={ {color: 'inherit', textDecoration: 'inherit'}}>Peliculas</Link>
-            <Link to="" className="links" style={ {color: 'inherit', textDecoration: 'inherit'}}>Contacto</Link>
+       
+        
+<div>
+<nav class="navbar navbar-expand-lg bg-body-tertiary header"  >
+<div class="container-fluid" >
+<img className="logoreacflix" src="./assets/reacflixlogo.svg"/>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse hamburgernav" id="navbarNavAltMarkup">
+    <div class="navbar-nav headertitulos">
+      <Link to="" className="links" class="nav-link" style={ {color: 'inherit', textDecoration: 'inherit'}}>Peliculas</Link>
+      <Link to="" className="links" class="nav-link" style={ {color: 'inherit', textDecoration: 'inherit'}}>Contacto</Link>
   
-            <Link to="login" className="links" style={ { color: 'inherit', textDecoration: 'inherit'}}>Login</Link>
-            <Link to="login" className="links" style={ { color: 'inherit', textDecoration: 'inherit'}}>Log-out</Link>
-            
-        </div>
+  <Link to="login" className="links" class="nav-link" style={ { color: 'inherit', textDecoration: 'inherit'}}>Login</Link>
+  <Link to="login" className="links" class="nav-link" style={ { color: 'inherit', textDecoration: 'inherit'}}>Log-out</Link>
+    </div>
+  </div>
+</div>
+</nav>
+</div>
+
     )
 
 }
