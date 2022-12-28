f2dce17e6b85acda3f24f0a5a8980f34a0b4cd1e
Linda M De Marco <maribeldemarco@gmail.com>
2022-12-28 17:51:51 -0300

    se hacen cambios para menu hamburguesa

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,15 +6,27 @@ import Logout from "./Logout";
 function Header() {
 
     return (
-        <div className="header">
-            <img className="logoreacflix" src="./assets/reacflixlogo.svg" />
-            <Link to="/" className="links" style={{ color: 'inherit', textDecoration: 'inherit' }}>Peliculas</Link>
-            <Link to="/form" className="links" style={{ color: 'inherit', textDecoration: 'inherit' }}>Contacto</Link>
-            <NavLink to="/poster" className="links">
-                <i className="fa-regular fa-image"></i> Wallpaper
-            </NavLink>
+              
+<div>
+<nav class="navbar navbar-expand-lg bg-body-tertiary header"  >
+<div class="container-fluid" >
+<img className="logoreacflix" src="./assets/reacflixlogo.svg"/>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse hamburgernav" id="navbarNavAltMarkup">
+    <div class="navbar-nav headertitulos">
+    <Link to="/" className="links" style={{ color: 'inherit', textDecoration: 'inherit' }}>Peliculas</Link>
+    <Link to="/form" className="links" style={{ color: 'inherit', textDecoration: 'inherit' }}>Contacto</Link>  
+    <NavLink to="/poster" className="links">
+    <i className="fa-regular fa-image"></i> Wallpaper
+    </NavLink>
             <Logout />
-        </div>
+    </div>
+  </div>
+</div>
+</nav>
+</div>
     )
 
 }
