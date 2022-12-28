5eb8492caf2fe3973d357c730ec66489dea2c819
Linda M De Marco <maribeldemarco@gmail.com>
2022-12-28 20:58:34 -0300

    se modifica header y footer haciendolo responsive y se agrega menu hamburguesa

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ function Footer() {
   return (
     <div className="footer">
 
-      <Link to="/poster" className="links" style={{ color: 'inherit', textDecoration: 'inherit' }}>Wallpaper</Link>
+      <Link to="/poster" className="links" style={{ color: 'inherit', textDecoration: 'inherit' }}> <p>Wallpaper</p></Link>
 
-      <div className='contacto'> Contacto
+      <div className='contacto'> <p>Contacto</p>
         <a href="https://github.com/ccmansilla/22804-grupo4-movies" target="_blank" className="links" style={{ color: 'inherit', textDecoration: 'inherit' }}>
           <div>
             <i className="fa-brands fa-github fonticon " ></i> <p>Github</p>
@@ -16,10 +16,11 @@ function Footer() {
         </a>
       </div>
 
-      <div className="btn-group">
+      
         <button className="btn btn-secondary btn-lg dropdown-toggle nuestroequipo" type="button" data-bs-toggle="dropdown" aria-expanded="false">
           <p>Nuestro equipo</p>
         </button>
+
         <ul class="dropdown-menu">
           <li><a className="dropdown-item" href="https://www.linkedin.com/in/carloscmansilla/"><i className="fa-brands fa-linkedin fonticon " ></i>Carlos Mansilla</a></li>
           <li><a className="dropdown-item" href="http://www.linkedin.com/in/ferffloress"><i className="fa-brands fa-linkedin fonticon " ></i> Fernanda Flores</a></li>
@@ -28,7 +29,7 @@ function Footer() {
           <li><a className="dropdown-item" href="https://www.linkedin.com/in/maribel-demarco/"><i className="fa-brands fa-linkedin fonticon " ></i> Maribel De Marco</a></li>
 
         </ul>
-      </div>
+     
 
     </div>
 
